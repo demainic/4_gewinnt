580195ef8592d00f508a4612b934527ec4326155
Nicolas <demainic@zhaw.students.ch>
2022-11-29 11:58:38 +0100

    Aufgabe 2

--- Aufgabe 2/vierGewinnt.js
+++ Aufgabe 2/vierGewinnt.js
@@ -1,3 +1,7 @@
+const COLUMNS = 6;
+const ROWS = 7;
+let game = Array(COLUMNS).fill('').map(el => Array(ROWS).fill(''))
+
 function elt (type, attrs, ...children) {
     let node = document.createElement(type)
     for (a in attrs) {
@@ -11,13 +15,15 @@ function elt (type, attrs, ...children) {
 }
 
 function showBoard (){
-    let rowsColsCount = 42
-    for (let i = 0; i < rowsColsCount; i++) {
-        let rand = Math.floor(Math.random() * (3 - 1 + 1))+1 // max-min+1 + min
-        if (rand == 1){
+    reset()
+    let board = document.querySelector('.board')
+    let flatGame = game.flat()
+    for (let i = 0; i < flatGame.length; i++) {
+
+        if (flatGame[i] == "r"){
             child = elt('div', {class: "red piece"})
             document.querySelector('.board').appendChild(elt('div', {class:'field'}, child))
-        }else if (rand == 2){
+        }else if (flatGame[i] == "b"){
             child = elt('div', {class: "blue piece"})
             document.querySelector('.board').appendChild(elt('div', {class:'field'}, child))
         }else{
@@ -25,4 +31,27 @@ function showBoard (){
         }
 
     }
+}
+
+function reset(){
+    let board = document.querySelector('.board')
+    board.innerHTML = ""
+
+}
+function changeByInterval() {
+    let randColor = Math.floor(Math.random() * (3 - 1 + 1))+1 // max-min+1 + min
+    let randPlace = Math.floor(Math.random() * COLUMNS * ROWS)
+    let y = Math.trunc(randPlace / ROWS)
+    let x = (randPlace % ROWS)
+
+    if (randColor == 1){
+        game[y][x] = "r"
+    }else if (randColor == 2){
+        game[y][x] = "b"
+    }else {
+        game[y][x] = ""
+    }
+
+
+    showBoard()
 }
