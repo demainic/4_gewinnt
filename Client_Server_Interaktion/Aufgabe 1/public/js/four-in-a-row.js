6f47d26a5b95031e533b71e454ded2ede26530b2
Elia Bhend <bugohoss@me.com>
2022-12-23 16:08:27 +0100

    Update

--- Client_Server_Interaktion/Aufgabe 1/public/js/four-in-a-row.js
+++ Client_Server_Interaktion/Aufgabe 1/public/js/four-in-a-row.js
@@ -1,5 +1,5 @@
 import { render } from "./lib/suiweb.js"
-import { connect4Winner} from "./connect4-winner.js";
+import { connect4Winner } from "./connect4-winner.js";
 
 const COLUMNS = 6;
 const ROWS = 7;
@@ -8,7 +8,7 @@ const URL = "http://localhost:3000/api/data/state?api-key=c4game"
 
 let state = {			
 	board: Array(COLUMNS).fill('').map(el => Array(ROWS).fill('')),
-	currentPlayer: 'b',
+	currentPlayer: "r",
 }
 
 const App = () => [Board, { board: state.board }]
@@ -23,20 +23,20 @@ const Board = ({ board }) => {
 
 const Field = ({ type, index }) => {
     if (type == '') {
-        return ["div", { className: "field " + index }]
+        return ["div", { className: "field " + index, id: index  }]
     }
 
     if (type == 'r') {
-        return ["div", { className: "field " + index }, ["div", { className: "red piece" }]]
+        return ["div", { className: "field " + index, id: index  }, ["div", { className: "red piece" }]]
     }
 
     if (type == 'b') {
-        return ["div", { className: "field " + index }, ["div", { className: "blue piece" }]]
+        return ["div", { className: "field " + index, id: index }, ["div", { className: "blue piece" }]]
     }
 }
 
 function showBoard() {
-    const app = document.querySelector(".board")
+    const app = document.querySelector(".suiweb")
     render([App], app)
     return app
 }
@@ -61,29 +61,54 @@ function buttonEventHandler(){
 }
 
 function boardEventHandler(){
-    document.querySelector('.board').addEventListener("click", () => {
-    console.log("board event listener clicked");
+    document.querySelector('.board').addEventListener("click", (element) => {        
+        doTurn(element)
+        checkIfGameOver()
     })
 }
 
 
-function doTurn(){
-    if(state.currentPlayer === 'r'){  }
+function doTurn(element){
+    let fieldNumber = element.target.id;
+    let field = Math.floor(fieldNumber % COLUMNS);
+    let row = Math.floor(fieldNumber / ROWS);
+    
+    if(!checkIfEmptyCell(field, row)) {
+        alert("This cell is already used")
+    }
+    else {
+        state.board[field][row] = getCurrentPlayer()
+        state.currentPlayer === "r" ? state.currentPlayer = "b" : state.currentPlayer = "r"
+    }
+     
+    console.log(state.board)
+    setColorInTitle();   
 }
 
+function checkIfGameOver(){
+    return connect4Winner(getCurrentPlayer, state.board) ? alert("Game finished") : '';
+}
 
-function setColorAndPlayer() {
+function checkIfEmptyCell(field , row){
+    return state.board[field][row] === "";
+}
+
+function getCurrentPlayer(){
+    return state.currentPlayer === 'r' ? 'r' : 'b' 
+}
+
+function setColorInTitle() {
     let title = document.getElementById('header-title')
-    if (state.currentPlayer === "b") {
+    if (state.currentPlayer === "b" ) {
         title.textContent = "Rot ist am Zug"
         title.style.backgroundColor = "red"
     } else {
         title.textContent = "Blau ist am Zug"
         title.style.backgroundColor = "blue"
+        state.currentPlayer = "r"
     }
 
 }
-
 
 function saveStateToServer() {
     fetch(URL, {
@@ -93,9 +118,13 @@ function saveStateToServer() {
     }).then(result => console.log(result))
 }
 
+function saveStateToLocalStoreage(){
+    localStorage.setItem("state", JSON.stringify(state));
+}
+
 function loadStateFromLocalStorage(){
- if (localStorage.getItem("board") != null) {
-    state.board = JSON.parse(localStorage.getItem("board"));
+ if (localStorage.getItem("state") != null) {
+    state.board = JSON.parse(localStorage.getItem("state"));
  }
  }
 
@@ -109,13 +138,18 @@ function loadStateFromLocalStorage(){
  }
 
  function restartGame() {
+    let title = document.getElementById('header-title')
+    if (state.currentPlayer === "b" ) {
+        title.textContent = "Rot ist am Zug"
+        title.style.backgroundColor = "red"
+    }
     state.board = Array(COLUMNS).fill('').map(el => Array(ROWS).fill(''))
  }
 
  function init(){
 	showBoard()
-	buttonEventHandler()
     boardEventHandler()
+	buttonEventHandler()
 }
 
 init()
