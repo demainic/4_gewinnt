1f69dc3ed65f78b051d6e8fa8de0fd1fd60b6fd3
Elia Bhend <bugohoss@me.com>
2022-12-23 21:05:09 +0100

    Changes

--- Client_Server_Interaktion/Aufgabe 1/public/js/four-in-a-row.js
+++ Client_Server_Interaktion/Aufgabe 1/public/js/four-in-a-row.js
@@ -1,13 +1,13 @@
 import { render } from "./lib/suiweb.js"
 import { connect4Winner } from "./connect4-winner.js";
 
-const COLUMNS = 6;
-const ROWS = 7;
+const COLUMNS = 7;
+const ROWS = 6;
 const URL = "http://localhost:3000/api/data/state?api-key=c4game"
 
 
 let state = {
-	board: Array(COLUMNS).fill('').map(el => Array(ROWS).fill('')),
+	board: Array(ROWS).fill('').map(el => Array(COLUMNS).fill('')),
 	currentPlayer: "red",
 }
 
@@ -16,6 +16,8 @@ let stateseq = {
     player: [],
     current: 0,
 }
+
+let title = document.getElementById('header-title')
 
 const App = () => [Board, { board: state.board }]
 
@@ -34,7 +36,7 @@ const Field = ({ type, index }) => {
 }
 
 function showBoard() {
-    const app = document.querySelector(".suiweb")
+    const app = document.querySelector(".board")
     render([App], app)
     return app
 }
@@ -55,8 +57,8 @@ function buttonEventHandler(){
     document.getElementById('button-new-game').addEventListener("click", () => {
 		restartGame()
 	})
-    document.getElementById('button-undo').addEventListener("click", () => {
-        undo()
+    document.getElementById('button-undo-last-move').addEventListener("click", () => {
+        undoLastMove()
     })
 
 }
@@ -69,36 +71,35 @@ function boardEventHandler(){
 
 function doTurn(element){
     let fieldNumber = element.target.id;
-    let field = Math.floor(fieldNumber % COLUMNS);
-    let row = Math.floor(fieldNumber / ROWS);
-    let title = document.getElementById('header-title')
-    if(!checkIfEmptyCell(field, row)) {
+    let column = fieldNumber % COLUMNS;
+    let row = Math.floor(fieldNumber / COLUMNS);
+
+    if(checkIfOccupiedCell(row,column)){
         alert("This cell is already used");
     }
     else if(!connect4Winner(getOtherPlayer(), state.board)) {
-        state.board[field][row] = getCurrentPlayer();
+        state.board[row][column] = getCurrentPlayer();
 
-        let pieceElement = ["div",	{className: state.board[field][row] + " piece"}];
+        let pieceElement = ["div",	{className: state.board[row][column] + " piece"}];
         let root = document.querySelector('[class="field ' + fieldNumber + '"]');
 
         stateseq.element.push(element)
         stateseq.player.push(state.currentPlayer)
         stateseq.current +=1
 
-        console.log(stateseq)
-        console.log(stateseq.list)
         changeCurrentPlayer();
         render(pieceElement, root);
         setWinTitle();
     }
-
     else {
-        alert("Game over!")
+        showMessage(getCurrentPlayer + "hat gewonnen!")
+        alert("Game")
+        restartGame()
     }
 }
 
-function checkIfEmptyCell(field , row){
-    return state.board[field][row] === "";
+function checkIfOccupiedCell(row , column){
+    return state.board[row][column] !== '';
 }
 
 function getCurrentPlayer(){
@@ -113,7 +114,6 @@ function getOtherPlayer(){
     }
 }
 function setWinTitle(){
-    let title = document.getElementById('header-title')
     if (connect4Winner("red", state.board)) {
         title.textContent = "Rot hat gewonnen"
         title.style.backgroundColor = "red"
@@ -123,8 +123,8 @@ function setWinTitle(){
         title.style.backgroundColor = "blue"
     }
 }
+
 function changeCurrentPlayer() {
-    let title = document.getElementById('header-title')
     if (state.currentPlayer === "blue") {
         title.textContent = "Rot ist am Zug"
         title.style.backgroundColor = "red"
@@ -153,58 +153,60 @@ function saveStateToLocalStoreage(){
 }
 
 function loadStateFromLocalStorage(){
- if (localStorage.getItem("state") != null) {
-    state = JSON.parse(localStorage.getItem("state"));
- }
+    if (localStorage.getItem("state") != null) {
+        state = JSON.parse(localStorage.getItem("state"));
+    }
  }
 
  function loadStateFromServer(){
     fetch(URL)
     .then((response) => response.json())
     .then((data) => {
-            state = data;
-            showBoard();
+            state = data
+
     })
  }
 
  function restartGame() {
-     let title = document.getElementById('header-title')
      let board = document.querySelector('.board')
      board.innerHTML = ""
-    state.board = Array(COLUMNS).fill('').map(el => Array(ROWS).fill(''))
-    state.currentPlayer = "red"
+     state.board = Array(ROWS).fill('').map(el => Array(COLUMNS).fill(''))
+     state.currentPlayer = "red"
      title.textContent = "Rot ist am Zug"
      title.style.backgroundColor = "red"
      init()
  }
 
- function undo() {
+ function showMessage(message){
+    title.textContent = message;
+ }
+
+ function undoLastMove() {
     if (stateseq.current > 0) {
         stateseq.current -= 1
         let element = stateseq.element.pop()
         let fieldNumber = element.target.id;
-        let field = Math.floor(fieldNumber % COLUMNS);
-        let row = Math.floor(fieldNumber / ROWS);
-        state.board[field][row] = ""
+        let column = fieldNumber % COLUMNS;
+        let row = Math.floor(fieldNumber / COLUMNS);
+
+        state.board[row][column] = ""
         element.target.innerHTML = ""
         changeCurrentPlayer()
-
-
     }
     else {
-        alert("No more undo possible")
+        showMessage("No more undo possible")
     }
  }
 
+ function reInitView(){
+  if (!state.winner) {
+    state.board = document.querySelector(".board");
+  }
+
+ }
  function init(){
 	showBoard()
     boardEventHandler()
 	buttonEventHandler()
 }
-
-
 init()
-
-
-
-
